df18d138f3a98b51df07034ab58cb29976d83ed6
Alexandre Julião <alexandre.juliao@grupoepea.com.br>
2022-05-03 09:36:56 -0300

    create closeButton - 30min

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,8 +1,17 @@
+import { CloseButton } from "./CloseButton";
+
+const feedbackTypes = {
+    BUG:{title: 'Problema'},
+    IDEIA:{title: 'Idéia'},
+    OTHER:{title: 'Outro'},
+}
+
 export function WidgetForm(){
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2em)] md:w-auto">
             <header>
                 <span className="text-xl leading-6">Deixe seu feedback</span>
+                <CloseButton/>
             </header>
             <p>Hello</p>
             <footer className="text-xs text-neutral-400">
