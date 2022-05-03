de19eb3a2e60b7099d9763e30fc36a60b7d90edd
Alexandre Julião <alexandre.juliao@grupoepea.com.br>
2022-05-03 09:20:36 -0300

    add header footer form

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -10,16 +10,16 @@ export function Widget() {
     setIsWidgetOpen(!isWidgetOpen)
   }
   return (
-    <div className='absolute bottom-5 right-5 flex flex-col items-end'>
-      {isWidgetOpen && <WidgetForm/>}
-      <button onClick={toggleWidgetVisibility} className='bg-brand-500 rounded-full px-3 h-12 text-white  flex items-center group'>
+    <Popover className='absolute bottom-4 right-4 md:right-10 flex flex-col items-end'>
+      <Popover.Panel><WidgetForm/></Popover.Panel>
+      <Popover.Button onClick={toggleWidgetVisibility} className='bg-brand-500 rounded-full px-3 h-12 text-white  flex items-center group'>
         <ChatTeardropDots className='w-6 h-6' />
 
         <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
           <span className='pl-2'></span>
           Feedback
         </span>
-      </button >
-    </div>
+      </Popover.Button >
+    </Popover>
   )
 }
